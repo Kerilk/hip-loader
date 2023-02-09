a3118f7d3a4f3442e8c216851beaba944725faf4
Brice Videau <bvideau@anl.gov>
2023-02-09 16:33:02 -0600

    Fixed build script.

--- src/build.sh
+++ src/build.sh
@@ -1,5 +1,8 @@
+rm -rf include-modified
+cp -r ../include include-modified
+cat headers.patch | patch -i - -d include-modified/ -s -p1
 ruby extract_hip.rb
 ruby gen_hip_dipatch_table.rb | indent -bfda -l160 > hip_dispatch.h
 ruby gen_hip_dispatch_stubs.rb | indent -bfda -l160 > hip_dispatch_stubs.h
-gcc -fPIC -c hip_loader.c -I include -D__HIP_PLATFORM_AMD__ -Wall
+gcc -fPIC -c hip_loader.c -I ../include -D__HIP_PLATFORM_AMD__ -Wall
 gcc -fPIC -shared -Wl,--version-script -Wl,hip.map hip_loader.o -Wl,-soname -Wl,libHIPLoader.so.5 -o libHIPLoader.so.5.4.0
