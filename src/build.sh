02c6e99449b420a5f874da34421e210aeb0d16bb
Brice Videau <bvideau@anl.gov>
2023-08-07 22:45:13 +0000

    Add symbolic links, debug symbols, and remove dependency on indent.

--- src/build.sh
+++ src/build.sh
@@ -2,7 +2,9 @@ rm -rf include-modified
 cp -r ../include include-modified
 cat headers.patch | patch -i - -d include-modified/ -s -p1
 ruby extract_hip.rb
-ruby gen_hip_dipatch_table.rb | indent -bfda -l160 > hip_dispatch.h
-ruby gen_hip_dispatch_stubs.rb | indent -bfda -l160 > hip_dispatch_stubs.h
-gcc -fPIC -c hip_loader.c -I ../include -D__HIP_PLATFORM_AMD__ -Wall
-gcc -fPIC -shared -Wl,--version-script -Wl,hip.map hip_loader.o -Wl,-soname -Wl,libHIPLoader.so.5 -lpthread -ldl -o libHIPLoader.so.5.4.0
+ruby gen_hip_dipatch_table.rb > hip_dispatch.h
+ruby gen_hip_dispatch_stubs.rb > hip_dispatch_stubs.h
+gcc -fPIC -g -c hip_loader.c -I ../include -D__HIP_PLATFORM_AMD__ -Wall
+gcc -fPIC -g -shared -Wl,--version-script -Wl,hip.map hip_loader.o -Wl,-soname -Wl,libHIPLoader.so.5 -lpthread -ldl -o libHIPLoader.so.5.4.0
+ln -f -s libHIPLoader.so.5.4.0 libHIPLoader.so
+ln -f -s libHIPLoader.so.5.4.0 libHIPLoader.so.5
